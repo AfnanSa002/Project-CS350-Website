68b80ef51c1f890398974354dcd9812fc79ec76f
JMNewsome <jmnewsome@yahoo.com>
2022-12-09 17:38:56 -0500

    "Update Report Site"

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"odu.edu.cs.cs350.pne"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"odu.edu.cs.cs350.pne"}]
